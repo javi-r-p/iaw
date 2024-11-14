00d25e91bd1a8e572aa411db85309341bf75398f
javi-r-p <7460103@alu.murciaeduca.es>
2024-11-14 18:04:44 +0100

    UT3 Iniciada

--- UT2-PHP/P2.1/convertirEuros.php
+++ UT2-PHP/P2.1/convertirEuros.php
@@ -3,37 +3,40 @@
 		<title>Conversión de divisas</title>
 		<meta charset="UTF-8">
 		<?php
-			$divisas = array("dolar"=>"1.11", "dolar"=>"0.83", "rublo"=>"118.44", "peso"=>"22.97", "rupia"=>"84.26", "franco"=>"1.02", "dinar"=>"157.78", "libra"=>"15.69");
-			function calcularDivisas ($cantidad, $divisa) {
-				$valorDivisaStr = $divisas['$divisa'];
-				$valorDivisa = floatval($valorDivisaStr);
-				$conversion = $valorDivisa*$cantidad;
-				return $conversion;
+			$divisas = array("dolar"=>"1.11", "libra"=>"0.83", "rublo"=>"118.44", "peso"=>"22.97", "rupia"=>"84.26", "franco"=>"1.02", "dinar"=>"157.78", 
+			"lira"=>"15.69");
+			function conversion($cantidad, $mon) {
+				global $divisas;
+				global $divisa;
+				if (array_key_exists($divisa, $divisas)) {
+					$dinero = $cantidad * $divisas[$mon];
+				} else {
+					$dinero = "No hay datos de esa moneda.";
+				}
+				return $dinero;
 			}
 		?>
 	</head>
 	<body>
 		<?php
-			echo $_SERVER['PHP_SELF'];
 			if (isset($_GET['enviar'])) {
-				$euros = floatval($_GET['euros']);
+				$euros = $_GET['euros'];
+				$divisa = $_GET['divisa'];
 				echo "<h1>RESULTADO DE LA CONVERSIÓN</h1>\n";
-				echo $euros . " EUROS = " . calcularDivisas ($euros,$_GET['divisa']); . "\n";
-				echo "<br>\n";
-				echo "<h1>----- RESTO DIVISAS -----</h1>\n";
-				echo "<table>\n";
-				echo "<tr>\n";
-				echo "<th>DIVISA</th>\n";
-				echo "<th>CANTIDAD</th>\n";
-				echo "</tr>\n";
-				foreach ($divisas as $nombreDivisa => $valor) {
-					$funcion = calcularDivisas ($euros, $nombreDivisa);
-					echo "<tr>\n";
-					echo "<td>" . $nombreDivisa . "</td>\n";
-					echo "<td>" . $funcion . "</td>\n";
-					echo "</tr>\n";
+				$resultado = conversion($euros, $divisa);
+				echo "$euros EUROS = $resultado $divisa.\n";
+				unset($resultado);
+				echo "<br>";
+				echo "<h1> ----- RESTO DE DIVISAS -----</h1>\n";
+				echo "<table border='1'>\n";
+				foreach ($divisas as $moneda => $valor) {
+					if ($moneda != $divisa) {
+						$resultado = conversion($euros, $moneda);
+						echo "<tr><td>$resultado</td><td>$moneda</td></tr>\n";
+					}
 				}
 				echo "</table>\n";
+				echo "<p><a href=" . $_SERVER['PHP_SELF'] . ">Volver al formulario</a></p>";
 			} else {
 				echo '<form method="GET" action="' . $_SERVER['PHP_SELF'] . '">' . "\n";
 				echo "<label>CANTIDAD EN EUROS:</label>\n";
@@ -51,7 +54,6 @@
 				echo "</select>\n";
 				echo '<input type="submit" name="enviar" value="Convertir">' . "\n";
 				echo "</form>\n";
-				echo "<p><a href=" . $_SERVER['PHP_SELF'] . ">Volver al formulario</a></p>";
 			}
 		?>
 	</body>
