a498172e2bc2a11280b0fbefb968066116e44d9d
Javier Rodríguez Parra <117043352+javi-r-p@users.noreply.github.com>
2024-12-07 13:18:25 +0100

    Ahora el proceso de crear un empleado es más rápido y automático

--- UT3-PHPBBDD/26.11.2024/gestionarEmpleados.php
+++ UT3-PHPBBDD/26.11.2024/gestionarEmpleados.php
@@ -84,7 +84,11 @@
 						echo "<a href='" . $_SERVER['PHP_SELF'] . "'>Volver al inicio</a>\n";
 					} else {
 						echo "<form action='" . $_SERVER['PHP_SELF'] . "?accion=insertar&inicio=Iniciar" . "' method='POST'>\n";
-						echo "<label>Identificador: </label><input type='number' name='codigoEmpleado' value='" . $_POST['codigoEmpleado'] . "' required><br>\n";
+						$codigoEmpleado = mysqli_query($conexion, "SELECT CodigoEmpleado FROM Empleados ORDER BY CodigoEmpleado DESC LIMIT 1");
+						$ultimoCodigoEmpleado = mysqli_fetch_array($codigoEmpleado);
+						$ultimoCodigoEmpleado = $ultimoCodigoEmpleado['CodigoEmpleado'] + 1;
+						echo "<label>Identificador: </label><input type='number' name='codigoEmpleado' value='" . $ultimoCodigoEmpleado . "' min='" . $ultimoCodigoEmpleado . "'><br>\n";
+						mysqli_free_result($codigoEmpleado);
 						echo "<label>Nombre completo: </label><input type='text' name='nombre' required><br>\n";
 						echo "<label>Extensión: </label><input type='text' name='extension' required><br>\n";
 						echo "<label>Correo electrónico: </label><input type='text' name='email' required><br>\n";
@@ -134,18 +138,7 @@
 					}
 					break;
 				case "insertar":
-					if (isset($_POST['codigoEmpleado'])) {
-						insertar();
-					} else {
-						$codigoEmpleado = mysqli_query($conexion, "SELECT CodigoEmpleado FROM Empleados ORDER BY CodigoEmpleado DESC LIMIT 1");
-						$ultimoCodigoEmpleado = mysqli_fetch_array($codigoEmpleado);
-						$ultimoCodigoEmpleado = $ultimoCodigoEmpleado['CodigoEmpleado'] + 1;
-						mysqli_free_result($codigoEmpleado);
-						echo "<form action='" . $_SERVER['PHP_SELF'] . "?accion=insertar&inicio=Iniciar' method='POST'>\n";
-						echo "<label>Identificador: </label><input type='number' name='codigoEmpleado' value='" . $ultimoCodigoEmpleado . "' min='" . $ultimoCodigoEmpleado . "'><br>\n";
-						echo "<input type='submit' name='iniciarInsercion' value='Insertar'>\n";
-						echo "</form>\n";
-					}
+					insertar();
 					break;
 				/* case "modificar":
 					break;
