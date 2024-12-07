9514f3d368dd53e7407e166778f6ac57904cea1e
javi-r-p <7460103@alu.murciaeduca.es>
2024-12-07 09:18:28 +0000

    Gestión de empleados

--- UT3-PHPBBDD/26.11.2024/gestionarEmpleados.php
+++ UT3-PHPBBDD/26.11.2024/gestionarEmpleados.php
@@ -1,19 +1,102 @@
 <html>
-	<head>
-		<title></title>
-		<meta charset="UTF-8">
-		<style>
-			body {
-				font-family: Arial;
+<head>
+	<title>Gestión de empleados</title>
+	<meta charset="UTF-8">
+	<style>
+		body {
+			font-family: Arial;
+		}
+		table, th, td {
+			border: 1px solid black;
+			border-collapse: collapse;
+		}
+		th, td {
+			padding: 10px;
+		}
+	</style>
+	<?php
+		$conexion = mysqli_connect('localhost', 'modf', 'modf', 'jardineria');
+	?>
+</head>
+<body>
+	<h1>Gestión de empleados</h1>
+	<hr>
+	<?php
+		if (isset($_GET['inicio'])) {
+			$accion = $_GET['accion'];
+			function consultar ($codigoEmpleado) {
+				echo "<h2>Consulta de empleado</h2>\n";
+				global $conexion;
+				$consulta = mysqli_query($conexion, "SELECT * FROM Empleados WHERE CodigoEmpleado = $codigoEmpleado");
+				if (mysqli_num_rows($consulta) == 0) {
+					echo "<h3>No existe ningún empleado con el código " . $codigoEmpleado . "</h3>\n";
+					echo "<a href='" . $_SERVER['PHP_SELF'] . "?accion=consultar&inicio=iniciar'>Ejecutar otra consulta</a>\n";
+				} else {
+					$empleado = mysqli_fetch_array($consulta);
+					echo "<table>\n";
+					echo "<tr>\n";
+					echo "<th>Código del empleado</th>\n";
+					echo "<th>Nombre completo</th>\n";
+					echo "<th>Extensión</th>\n";
+					echo "<th>Correo electrónico</th>\n";
+					echo "<th>Código de la oficina</th>\n";
+					echo "<th>Código de jefe</th>\n";
+					echo "<th>Puesto</th>\n";
+					echo "</tr>\n";
+					echo "<tr>\n";
+					echo "<td>" . $empleado['CodigoEmpleado'] . "</td>\n";
+					echo "<td>" . $empleado['Nombre'] . " " . $empleado['Apellido1'] . " " . $empleado['Apellido2'] . "</td>\n";
+					echo "<td>" . $empleado['Extension'] . "</td>\n";
+					echo "<td>" . $empleado['Email'] . "</td>\n";
+					echo "<td>" . $empleado['CodigoOficina'] . "</td>\n";
+					if ($empleado['CodigoJefe'] == NULL) {
+						echo "<td> - </td>\n";
+					} else {
+						echo "<td>" . $empleado['CodigoJefe'] . "</td>\n";
+					}
+					echo "<td>" . $empleado['Puesto'] . "</td>\n";
+					echo "</tr>\n";
+					echo "</table>\n";
+					echo "<hr>\n";
+					echo "<a href='" . $_SERVER['PHP_SELF'] . "?accion=consultar&inicio=iniciar'>Consultar otro empleado</a>\n";
+					echo "<br>\n";
+					echo "<a href='" . $_SERVER['PHP_SELF'] . "'>Volver al inicio</a>\n";
+				}
 			}
-		</style>
-		<?php
-			$conexion = mysqli_connect('localhost', 'modf', 'modf', 'jardineria');
-		?>
-	</head>
-	<body>
-		<?php
-			mysqli_close($conexion);
-		?>
-	</body>
+			switch ($accion) {
+				case "consultar":
+					if (isset($_POST['ejecutarConsulta'])) {
+						consultar ($_POST['codigoEmpleado']);
+					} else {
+						echo "<form action='" . $_SERVER['PHP_SELF'] . "?accion=consultar&inicio=iniciar' method='POST'>\n";
+						echo "<label>Código del empleado: </label><input type='number' name='codigoEmpleado' required>\n";
+						echo "<input type='submit' name='ejecutarConsulta' value='Consultar'>\n";
+						echo "</form>";
+					}
+					break;
+				/* case "insertar":
+					break;
+				case "modificar":
+					break;
+				case "eliminar":
+					break; */
+				default:
+					echo "<h2>La opción que has seleccionado aún no está disponible.</h2>\n";
+					echo "<a href='" . $_SERVER['PHP_SELF'] . "'>Volver al inicio</a>\n";
+					break;
+			}
+		} else {
+	?>
+		<form action="<?php $_SERVER['PHP_SELF'] ?>" method="GET">
+			<input type="radio" name="accion" value="consultar" required><label>Consultar</label><br>
+			<input type="radio" name="accion" value="insertar"><label>Insertar</label><br>
+			<input type="radio" name="accion" value="modificar"><label>Modificar</label><br>
+			<input type="radio" name="accion" value="eliminar"><label>Eliminar</label><br>
+			<input type="submit" name="inicio" value="Iniciar">
+		</form>
+	<?php
+		}
+		mysqli_close($conexion);
+	?>
+</body>
 </html>
